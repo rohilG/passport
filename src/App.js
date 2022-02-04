7f4cb24975fda1fa8c0acf402f541da261d92bfa
Rohil Gupta <rohilvg@gmail.com>
2022-02-04 05:14:56 -0500

    some api garbage + qr garbage ... shoutout 5am

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Verifier from "./views";
 
 function App() {
-  const nft_port_api_key = "cb0130f1-d009-4f9d-9f5b-f9c6471ebf38";
   const [currentAccount, setCurrentAccount] = useState();
 
   const connectWallet = async () => {
--- src/views/Verifier.js
+++ src/views/Verifier.js
@@ -1,37 +1,212 @@
 import React from "react";
-
-const verifyTransaction = async function (attendee_addr, creator_addr) {};
+import QrReader from "react-qr-reader";
+import QRCode from "react-qr-code";
+// import { useMoralis, useMoralisWeb3Api } from "react-moralis";
+import { FcCancel, FcTouchscreenSmartphone } from "react-icons/fc";
+import { Checkmark } from "react-checkmark";
+import { padding } from "@mui/system";
 
 class Verifier extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { artistPublicKey: "" };
+    this.state = {
+      attendee_addr: "",
+      creator_addr: "",
+      dataCollectionStep: 0,
+      mintDate: "",
+      isValidAttendee: false,
+    };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(event) {
-    verifyTransaction(this.state.artistPublicKey);
-    //alert("submitted: " + this.state.artistPublicKey);
-    event.preventDefault();
+  verifyAttendee() {
+    const nft_port_api_key = "cb0130f1-d009-4f9d-9f5b-f9c6471ebf38";
+    const moralis_api_key =
+      "gxpDcsiOxjhx1SvF5ROBiMVeeGsQ9WtZSo9mDB1nWpNf9hnd4aOpzU91A2jABQi0";
+    // const { initialize, isInitialized } = useMoralis();
+    // initialize({
+    //   appId: "ICYNGMsC7ru5DREZVwoWjX39UcYQo34HAsAIOQHo",
+    //   serverUrl: "https://hxzbhq0gqxph.usemoralis.com:2053/server",
+    // });
+    const nftport_headers = {
+      Authorization: nft_port_api_key,
+      //"Content-Type": "application/json",
+    };
+    const moralis_headers = {
+      "X-API-Key": moralis_api_key,
+      accept: "application/json",
+    };
+
+    fetch(
+      `https://api.nftport.xyz/v0/accounts/${this.state.creator_addr}?chain=ethereum&page_size=50`,
+      { nftport_headers }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          console.log(response);
+          throw new Error("Network response not OK");
+        }
+        response.json();
+      })
+      .then((data) => {
+        const creator_held_nfts = data["nfts"]
+          .filter((nft) => nft["creator_address"] === this.state.creator_addr)
+          .map((nft) => [nft["contract_address"], nft["token_id"]]);
+      })
+      .catch((error) => {
+        console.log("caught", error);
+        return false;
+      });
+
+    // fetch(
+    //   `https://deep-index.moralis.io/api/v2/nft/${this.state.attendee_addr}/transfers?chain=eth&format=decimal&direction=from`,
+    //   { moralis_headers }
+    // )
+    //   .then((response) => {
+    //     if (!response.ok) {
+    //       throw new Error("Network response not OK!");
+    //     }
+    //     response.json();
+    //   })
+    //   .then((data) => {
+    //     const attendee_sent_nfts = data["result"]
+    //       .filter((nft) => (nft["to_address"] === this.state.creator_addr && creator_held_nfts.includes([nft["token_address"], nft["token_id"]])));
+    //   })
+    //   .catch((error) => {
+    //     return false;
+    //   });
+
+    // attendee_sent_nfts.forEach((nft) => fetch(
+    //   `https://api.nftport.xyz/v0/nfts/${nft[0]}/${nft[1]}?chain=ethereum`,
+    //   { moralis_headers }
+    // )
+    //   .then((response) => {
+    //     if (!response.ok) {
+    //       throw new Error("Network response not OK!");
+    //     }
+    //     response.json();
+    //   })
+    //   .then((data) => {
+    //     if (data["nft"]["mint_date"] === this.state.mintDate) {
+    //       return true;
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     return false;
+    //   })
+    // )
+    // return false;
+
+    return true;
+    // const Web3Api = useMoralisWeb3Api();
+    // const options = {
+    //   chain: "eth",
+    //   address: attendee_addr,
+    //   limit: "10",
+    //   direction: "from",
+    // };
+    // const transfersNFT = await Web3API.account.getNFTTransfers(options);
   }
 
-  handleChange(event) {
-    this.setState({ artistPublicKey: event.target.value });
-  }
+  // handleSubmit(e) {
+  //   alert("submitted: " + this.state.creator_addr);
+  //   this.setState({ dataCollectionStep: 2 });
+  //   e.preventDefault();
+  // }
+
+  // handleChange(event) {
+  //   this.setState({ creator_addr: event.target.value });
+  // }
+
+  handleScan = (data) => {
+    if (data) {
+      console.log("data: ", data.split(":")[1]);
+
+      this.setState({ attendee_addr: data.split(":")[1] });
+
+      console.log(this.state.attendee_addr);
+      console.log(this.state.creator_addr);
+
+      const isValidAttendee = this.verifyAttendee();
+
+      console.log("isValidAttendee", isValidAttendee);
+
+      this.setState({
+        dataCollectionStep: 2,
+        isValidAttendee: isValidAttendee,
+      });
+    }
+  };
+
+  handleCreatorQRCode = (data) => {
+    if (data) {
+      alert("got some data", data);
+      this.setState({
+        creator_addr: data.split(" ")[0],
+        mintDate: data.split(" ")[1],
+        dataCollectionStep: 1,
+      });
+    }
+  };
 
   render() {
+    const { dataCollectionStep, attendee_addr, creator_addr, isValidAttendee } =
+      this.state;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label for="fname">Artist Creator:</label>
-        <br />
-        <input
-          type="text"
-          value={this.state.artistPublicKey}
-          onChange={this.handleChange}
-        />
-      </form>
+      <div>
+        {/* We generate this only when the artist mints their NFTs, and these QR codes are sent to each verifier */}
+        {/* This contains: creator_address + " " + mint_date */}
+
+        {dataCollectionStep === 0 && (
+          <div style={{ padding: "20px 20px" }}>
+            <QRCode value="0x1494bb40e35DF00afce4dd01Fe67d5eB1eA6AED6 2020-10-29T15:03:54.838612" />
+            <br />
+            {console.log("step 0")}
+
+            <QrReader
+              delay={300}
+              onScan={this.handleCreatorQRCode}
+              style={{ width: "30%", height: "30%" }}
+            />
+          </div>
+        )}
+
+        {/* {this.state.dataCollectionStep === 1 && (
+          <form onSubmit={this.handleSubmit}>
+            <label for="fname">Artist Creator:</label>
+            <br />
+            <input
+              type="text"
+              value={this.state.creator_addr}
+              onChange={this.handleChange}
+            />
+          </form>
+          <QrReader
+            delay={300}
+            onScan={this.handleScan}
+            style={{ width: "50%", height: "50%" }}
+          />
+        )}
+        */}
+
+        {dataCollectionStep === 1 && (
+          <QrReader
+            delay={300}
+            onScan={this.handleScan}
+            style={{ width: "50%", height: "50%" }}
+          />
+        )}
+
+        {dataCollectionStep === 2 &&
+          !isValidAttendee && <FcCancel /> &&
+          this.setState({ dataCollectionStep: 0 })}
+
+        {dataCollectionStep === 2 &&
+          isValidAttendee && <Checkmark /> &&
+          this.setState({ dataCollectionStep: 0 })}
+      </div>
     );
   }
 }
