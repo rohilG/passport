f33211cd72892023cda005f3edd6dbd95d8ca8e5
Rohil Gupta <rohilvg@gmail.com>
2022-02-05 01:22:27 -0500

    digital signature + generate QR code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Verifier from "./views";
+import Verifier from "./views/Verifier";
+import GeneratePubKeyQR from "./views/GeneratePubKeyQR";
 
 function App() {
-  const [currentAccount, setCurrentAccount] = useState();
-
-  const connectWallet = async () => {
-    try {
-      const { ethereum } = window;
-
-      if (!ethereum) {
-        alert("Get MetaMask!");
-        return;
-      }
-
-      const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
-
-      console.log("Connected", accounts[0]);
-      setCurrentAccount(accounts[0]);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Verifier />} />
         <Route path="verify" element={<Verifier />} />
+        <Route path="pubkeyproof" element={<GeneratePubKeyQR />} />
       </Routes>
     </BrowserRouter>
   );
