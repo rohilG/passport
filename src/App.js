a9d99040413dcf095b5d067e8854c7991813ef72
Rohil Gupta <rohilvg@gmail.com>
2022-02-05 04:42:03 -0500

    just a small refactor nothing important

--- src/App.js
+++ src/App.js
@@ -1,39 +1,65 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Verifier from "./views";
+import Header from "./Header";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import AccountContext from "./AccountContext";
+import AuthModalContext from "./AuthModalContext";
+import AuthModal from "./AuthModal";
+import RoutingSwitch from "./RoutingSwitch";
 
 function App() {
-  const [currentAccount, setCurrentAccount] = useState();
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [showAuthModal, setShowAuthModal] = useState(false);
 
-  const connectWallet = async () => {
+  const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window;
 
       if (!ethereum) {
-        alert("Get MetaMask!");
+        console.log("Make sure you have metamask!");
         return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
       }
 
-      const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
+      const accounts = await ethereum.request({ method: "eth_accounts" });
 
-      console.log("Connected", accounts[0]);
-      setCurrentAccount(accounts[0]);
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Verifier />} />
-        <Route path="verify" element={<Verifier />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthModalContext.Provider
+      value={{ show: showAuthModal, setShow: setShowAuthModal }}
+    >
+      <AccountContext.Provider
+        value={{ publicKey: currentAccount, setPublicKey: setCurrentAccount }}
+      >
+        <Router>
+          <Header />
+          <AuthModal />
+          <RoutingSwitch />
+          <Switch>
+            {/* <Route path="/" element={<Verifier />} /> */}
+            <Route path="verify" element={<Verifier />} />
+          </Switch>
+        </Router>
+      </AccountContext.Provider>
+    </AuthModalContext.Provider>
   );
 }
 
