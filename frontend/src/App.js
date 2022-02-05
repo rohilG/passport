bf011efd1c5131169d309f60c811cf20770cfa09
Aman Adhav <amanadhav@gmail.com>
2022-02-04 23:26:42 -0500

    login credentials created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,54 @@
 import Header from "./Header";
 import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
+import AccountContext from './AccountContext';
+import {useState, useContext, useEffect} from 'react';
+import AuthModalContext from './AuthModalContext';
+import AuthModal from './AuthModal';
 
 function App() {
+
+  const [currentAccount, setCurrentAccount] = useState('');
+  const [showAuthModal, setShowAuthModal] = useState(false);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }   
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+
   return (
-    <Router>
-      <Header />
-    </Router>
+    <AuthModalContext.Provider value={{show:showAuthModal, setShow: setShowAuthModal}}>
+      <AccountContext.Provider value={{publicKey: currentAccount, setPublicKey: setCurrentAccount}}>
+        <Router>
+          <Header />
+          <AuthModal/>
+        </Router>
+      </AccountContext.Provider>
+    </AuthModalContext.Provider>
   );
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {      
+      colors: {
+        auth_modal: {
+          DEFAULT: 'rgb(255, 255, 255)',
+        },
+        auth_modal_text: {
+          DEFAULT: 'rgb(0, 0, 0)',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
