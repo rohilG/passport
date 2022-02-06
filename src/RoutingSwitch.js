eeb3d1dfaf1d92f4bfb21efee921c66a6fbf2be1
Bob Wei <bobqywei@gmail.com>
2022-02-05 21:51:28 -0500

    first iteration qr auth

--- src/RoutingSwitch.js
+++ src/RoutingSwitch.js
@@ -4,6 +4,7 @@ import EventCreateModal from "./EventCreateModal";
 import EventListings from "./EventListings";
 import TicketPurchase from "./TicketPurchase";
 import Verifier from "./views/Verifier";
+import GeneratePubKeyQR from "./views/GeneratePubKeyQR";
 
 function RoutingSwitch() {
   let location = useLocation();
@@ -15,6 +16,7 @@ function RoutingSwitch() {
       <Route exact path="/attendee/tickets/:event_id" component={TicketPurchase} />
       <Route exact path="/artist/create/event" component={EventCreateModal} />
       <Route exact path="/verify" component={Verifier} />
+      <Route exact path="/attendee/qr" component={GeneratePubKeyQR} />
     </Switch>
   );
 }
