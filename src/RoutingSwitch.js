80c9e20dfd20dbf725abe4eef84387f5558d1609
Bob Wei <bobqywei@gmail.com>
2022-02-05 21:51:28 -0500

    first iteration qr auth

--- src/RoutingSwitch.js
+++ src/RoutingSwitch.js
@@ -2,6 +2,7 @@ import { Route, Switch, useLocation } from "react-router-dom";
 import AttendeeNftListings from "./AttendeeNftListings";
 import EventListings from "./EventListings";
 import Verifier from "./views/Verifier";
+import GeneratePubKeyQR from "./views/GeneratePubKeyQR";
 
 function RoutingSwitch() {
   let location = useLocation();
@@ -11,6 +12,7 @@ function RoutingSwitch() {
       <Route exact path="/" component={EventListings} />
       <Route exact path="/attendee/wallet" component={AttendeeNftListings} />
       <Route exact path="/verify" component={Verifier} />
+      <Route exact path="/attendee/qr" component={GeneratePubKeyQR} />
     </Switch>
   );
 }
